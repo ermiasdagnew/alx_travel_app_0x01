fde7c8b1a2ab6e7dfa0a3cc101802571d1703f8f
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-24 20:02:37 +0300

    Update urls.py

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -4,18 +4,24 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# Swagger Schema Configuration
 schema_view = get_schema_view(
-   openapi.Info(
-      title="ALX Travel API",
-      default_version='v1',
-      description="API documentation for Listings and Bookings",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Travel App API",
+        default_version='v1',
+        description="API documentation for Listings and Bookings",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('listings.urls')),  # include listings app API
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    # API Routes
+    path('api/', include('listings.urls')),
+
+    # Swagger Documentation
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-doc'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-doc'),
 ]
