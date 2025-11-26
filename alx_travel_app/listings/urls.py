114f463d1028f0f94d18de31306fb1d740d36005
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-26 10:08:10 +0300

    Update urls.py

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -6,3 +6,14 @@ router.register(r'listings', ListingViewSet)
 router.register(r'bookings', BookingViewSet)
 
 urlpatterns = router.urls
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet, basename='listings')
+router.register(r'bookings', BookingViewSet, basename='bookings')
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
